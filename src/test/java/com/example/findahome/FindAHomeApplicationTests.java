6c30d19fd7614f5a9376da6bf732eb22e3b945eb
DionChen <s8800242002@gmail.com>
2022-06-12 19:26:16 +0800

    init

---
+++ src/test/java/com/example/findahome/FindAHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.findahome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindAHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
